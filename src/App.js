5ab3ac26f1a54f5fd36600ebea25d08ceee72d66
flowers-energy <zachary.t.flowers@gmail.com>
2022-03-17 13:23:28 -0700

    lat and long completed

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ class App extends React.Component {
     super(props);
     this.state = {
       searchQuery: '',
-      itMe: {},
-      lat: {},
-      long: {}
+      city_name: '',
+      lat: '',
+      lon : ''
     }
   }
 
@@ -17,7 +17,10 @@ class App extends React.Component {
     const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.searchQuery}&format=json`
     const hereResponse = await axios.get(url);
     console.log(hereResponse.data[0]);
-    this.setState({itMe: hereResponse.data[0]});
+    this.setState({city_name: hereResponse.data[0].display_name});
+    this.setState({lat: hereResponse.data[0].lat});
+    this.setState({lon: hereResponse.data[0].lon});
+
   }
 
   render() {
@@ -26,14 +29,14 @@ class App extends React.Component {
         <input onChange={(event) => this.setState({searchQuery: event.target.value})}
           placeholder='Where would you like to go?'></input>
         <button onClick={this.goHere}>Explore!</button>
-        {this.state.itMe.display_name &&
-        <h2>Discover {this.state.itMe.display_name}</h2>
+        {this.state.city_name &&
+        <h2>Discover {this.state.city_name}</h2>
         }
-        {this.state.lat.latitude &&
-        <h3>{this.state.lat.latitude}</h3>
+        {this.state.lat &&
+        <h3>{this.state.lat}</h3>
         }
-        {this.state.long.longitude &&
-        <h3>{this.state.long.longitude}</h3>
+        {this.state.lon &&
+        <h3>{this.state.lon}</h3>
         }
       </div>
     );
