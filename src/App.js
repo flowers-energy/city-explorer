0a69fac4d189431e1c4c1e125ffa1e50790688f6
flowers-energy <zachary.t.flowers@gmail.com>
2022-03-17 12:52:18 -0700

    can return location sans lat and long

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchQuery: '',
+      itMe: {},
+      lat: {},
+      long: {}
+    }
+  }
+
+  goHere = async () => {
+    const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.searchQuery}&format=json`
+    const hereResponse = await axios.get(url);
+    console.log(hereResponse.data[0]);
+    this.setState({itMe: hereResponse.data[0]});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <input onChange={(event) => this.setState({searchQuery: event.target.value})}
+          placeholder='Where would you like to go?'></input>
+        <button onClick={this.goHere}>Explore!</button>
+        {this.state.itMe.display_name &&
+        <h2>Discover {this.state.itMe.display_name}</h2>
+        }
+        {this.state.lat.latitude &&
+        <h3>{this.state.lat.latitude}</h3>
+        }
+        {this.state.long.longitude &&
+        <h3>{this.state.long.longitude}</h3>
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
