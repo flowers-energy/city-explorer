e8d18485cb586b0e9ff505369f0bd080413d7cc3
flowers-energy <zachary.t.flowers@gmail.com>
2022-03-19 16:28:04 -0700

    map functioning

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import axios from 'axios';
+import Image from 'react-bootstrap/Image';
 
 class App extends React.Component {
   constructor(props) {
@@ -13,13 +14,16 @@ class App extends React.Component {
     }
   }
 
-  goHere = async () => {
+  goHere = async event => {
+    event.preventDefault()
     const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.searchQuery}&format=json`
     const hereResponse = await axios.get(url);
     console.log(hereResponse.data[0]);
-    this.setState({city_name: hereResponse.data[0].display_name});
-    this.setState({lat: hereResponse.data[0].lat});
-    this.setState({lon: hereResponse.data[0].lon});
+    this.setState({
+      city_name: hereResponse.data[0].display_name,lat:hereResponse.data[0].lat,
+      lon: hereResponse.data[0].lon
+    });
+    
 
   }
 
@@ -33,11 +37,15 @@ class App extends React.Component {
         <h2>Discover {this.state.city_name}</h2>
         }
         {this.state.lat &&
-        <h3>{this.state.lat}</h3>
+          <>
+            <h3>{this.state.lat}</h3>
+            <h3>{this.state.lon}</h3>
+            <Image src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&zoom=11&center=${this.state.lat},${this.state.lon}&width=200px&height=200px` }/>
+          </>
         }
-        {this.state.lon &&
-        <h3>{this.state.lon}</h3>
-        }
+        
+
+        
       </div>
     );
   }
